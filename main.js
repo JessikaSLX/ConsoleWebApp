af4cf0e0d9a6b31d5b40230f219f84df815e8f8c
JessikaSLX <89091806+JessikaSLX@users.noreply.github.com>
2021-10-09 19:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name = "Jessika";
+console.log(first_name);
+
+var last_name = "Maity";
+console.log(last_name);
+
+var full_name = first_name + last_name;
+console.log(full_name);
+
+var a = 10;
+console.log(a);
+
+var b = 5;
+console.log(b)
+
+var add = a + b;
+console.log(add);
